cdda4343c3857a6b01ca36ca46afc4247fe06f93
ccxxllhl2 <ccxxll23@163.com>
2023-10-18 11:44:57 +0800

    updated

--- esix.py
+++ esix.py
@@ -1,8 +1,18 @@
 import os
+import time
+import logging
 import subprocess
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 from mail import send_mail
+
+# Logging config
+logging.basicConfig(filename='history.log', 
+                    level=logging.INFO,
+                    format = '%(asctime)s  %(filename)s : %(levelname)s  %(message)s',
+                    datefmt = '%Y-%m-%d %A %H:%M:%S')
+
+logging.info('SAGA Running!')
 
 # DB Server Config
 org = "eSix"
@@ -17,11 +27,12 @@ teams_token = os.environ.get('eSixTeams')
 
 # devices {device_ip: [interface_id]}
 ## 192.168.245.69: [eth0]
-devices = {'192.168.245.69':['2', '3']}
+devices = {'192.168.245.69': 'eBridge2',
+           '192.168.245.68': 'eBridge1'}
 
 # SNMP walk cmd
-ifname_str = 'snmpwalk -v 2c -c SAGA {} IF-MIB::ifName.{}'
-ifstatus_str = 'snmpwalk -v 2c -c SAGA {} IF-MIB::ifOperStatus.{}'
+ifname_str = 'snmpwalk -v 2c -c SAGA {} IF-MIB::ifName'
+ifstatus_str = 'snmpwalk -v 2c -c SAGA {} IF-MIB::ifOperStatus'
 
 # Functions
 def write_ts_data(device, metric, value):
@@ -29,32 +40,37 @@ def write_ts_data(device, metric, value):
     api_writer.write(bucket=bucket, org=org, record=data_point)
 
 def snmp_get(cmd):
-    return filter(subprocess.check_output(cmd, shell=True).decode())
-
-def filter(data_str):
-    return data_str.split(': ')[1].split('\n')[0]
+    result = subprocess.check_output(cmd, shell=True).decode()
+    result_list = [line.split(': ')[1] for line in result.split('\n')[:-1]]
+    return result_list
 
 # esix
 class ESIX:
     def __init__(self):
         self.subject = 'SAGA Found eSix Device Interface Down!'
-        self.body = 'Device: {}\n - Interface {} is {}!\n\nThis is a test mail, do not reply!'
-        self.data = self.get_port_health()
+        self.body = 'Device: {}\n - Interface {} is {}'
+        #self.data = self.get_port_health()
 
     def get_port_health(self):
         data = {}
         for host in devices:
             data[host] = []
-            for if_id in devices[host]:
-                if_name = snmp_get(ifname_str.format(host, if_id))
-                if_status = snmp_get(ifstatus_str.format(host, if_id))
+            if_name_list = snmp_get(ifname_str.format(host))
+            if_status_list = snmp_get(ifstatus_str.format(host))
+            for i in range(len(if_name_list)):
+                if_name = if_name_list[i]
+                if_status = if_status_list[i]
                 if_status_int = int(if_status.split('(')[1].split(')')[0])
                 write_ts_data(host, if_name, if_status_int)
+                msg = self.body.format(devices[host], if_name, if_status[-3:])
                 if 'up' not in if_status:
-                    send_mail(subject=self.subject,
-                              body=self.body.format(host, if_name, if_status[-3:]))
+                    send_mail(subject=self.subject, body=msg)
+                logging.error(msg)
                 data[host].append({if_name: if_status})
         return data
     
 if __name__ == "__main__":
     esix = ESIX()
+    while True:
+        esix.data = esix.get_port_health()
+        time.sleep(60)
